5ccb9fb514da073d7c604c9e92c09abaeb9d9767
hoa0 <t-hoa.l@hotmail.com>
2022-11-04 11:40:13 +0100

    💡 fetch api

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,19 @@ import '../styles/style.css'
 
 // We can use node_modules directely in the browser!
 import * as d3 from 'd3';
-import count from './count.js';
 
-console.log('Hello, world!');
 
-let data = count(5, 5);
+const baseURL = "https://app.ticketmaster.com/discovery/v2/venues";
+const key = "?apikey=uC0UADyMdASdYwjLJRHfjH8AjPzRlhFF&locale=*";
+const endpoint = "https://app.ticketmaster.com/discovery/v2/venues?apikey=uC0UADyMdASdYwjLJRHfjH8AjPzRlhFF&locale=*";
 
-console.log(data);
+function getData() {
+  fetch(endpoint)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data?._embedded?.venues);
+      
+    });
+}
+
+getData();
