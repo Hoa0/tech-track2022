1efb190858c2501ddf675f1a3fef98cf7063123e
hoa0 <t-hoa.l@hotmail.com>
2022-11-06 13:02:50 +0100

    fetch variable

--- scripts/main.js
+++ scripts/main.js
@@ -5,15 +5,20 @@ import '../styles/style.css'
 import * as d3 from 'd3';
 
 
-const baseURL = "https://app.ticketmaster.com/discovery/v2/venues";
+const baseURL = "https://app.ticketmaster.com/discovery/v2";
+const venues = "/venues";
+const offers ="/offers";
+const events = "/events";
 const key = "?apikey=uC0UADyMdASdYwjLJRHfjH8AjPzRlhFF&locale=*";
-const endpoint = "https://app.ticketmaster.com/discovery/v2/venues?apikey=uC0UADyMdASdYwjLJRHfjH8AjPzRlhFF&locale=*";
+const endpoint = `${baseURL}${offers}${key}`;
+const countryEvents ="https://app.ticketmaster.com/discovery/v2/events.json?apikey=uC0UADyMdASdYwjLJRHfjH8AjPzRlhFF&locale=*&countryCode=NL";
 
 function getData() {
-  fetch(endpoint)
+  fetch(countryEvents)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data?._embedded?.venues);
+      //console.log(data?._embedded?.venues);
+      console.log(data?._embedded?.events);
       
     });
 }
